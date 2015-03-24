2fdbcb17d013a2cb2dcc68bbfdd82e08076865ea
Alyona <a.chornaya@gmail.com>
2015-03-24 02:51:41 +0200

    UI fixes

--- controllers/app.js
+++ controllers/app.js
@@ -35,6 +35,17 @@ app.config (['$routeProvider', function ($routeProvider) {
 
 }]);
 
+// directive for custom icon background
+app.directive('backImg', function(){
+    return function(scope, element, attrs){
+        var url = attrs.backImg;
+        element.css({
+            'background-image': 'url(images/icons-cat/' + url +')',
+            'background-size' : 'cover'
+        });
+    };
+});
+
 // global variables 
 //var apiBaseUrl = 'http://resxedit.com/api.php';
 var apiBaseUrl = 'api.php';
